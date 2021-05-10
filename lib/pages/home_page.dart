11c3ef947c169e23a2557e59b346c104dd99b9fd
Alex Li <github@alexv525.com>
2021-05-10 23:31:41 +0800

    ✨ Add url field and DApps grid placeholder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../constants/screens.dart';
+import '../extensions/extensions.dart';
 import 'web_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,13 +14,69 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   void _scan() {}
 
+  Widget _urlField(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      alignment: Alignment.center,
+      color: context.theme.dividerColor.withOpacity(0.1),
+      child: TextField(
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          isDense: true,
+          hintText: 'Enter website',
+        ),
+        style: context.textTheme.caption?.copyWith(fontSize: 14),
+        textAlign: TextAlign.center,
+        onSubmitted: (String url) {
+          if (!WebPage.linkRegExp.hasMatch(url)) {
+            return;
+          }
+          Navigator.of(context).pushNamed(
+            WebPage.routeName,
+            arguments: WebPageArguments(url: url),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _dAppGrid(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+      ),
+      itemCount: 12,
+      itemBuilder: (BuildContext c, int index) => Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: context.theme.dividerColor,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            const Text('DApp_title'),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Wallet4D'), centerTitle: true),
-      body: const WebPage(
-        url: 'https://github.com/Pocket4D/Wallet4D',
-        withAppBar: false,
+      backgroundColor: context.theme.cardColor,
+      body: Column(
+        children: <Widget>[
+          _urlField(context),
+          Expanded(child: _dAppGrid(context)),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _scan,
